5a9daf20448563a4dc90eb74e294c36e9d9bf593
autopp <autopp.inc@gmail.com>
2019-03-15 09:52:19 +0900

    feat: add AppButton as atoms

---
+++ components/atoms/AppButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="onClick">{{label}}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ""
+    }
+  },
+  methods: {
+    onClick(e) {
+      this.$emit("onClick")
+    }
+  }
+}
+</script>
