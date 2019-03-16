9ccfde1e1e1a77bfd7d469d0871dd85232951f54
autopp <autopp.inc@gmail.com>
2019-03-16 15:42:45 +0900

    feat: add number input as atom

---
+++ components/atoms/AppNumberInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <input type="number" v-model="internalValue" min="0" max="10" step="1" />
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    internalValue: {
+      get() {
+        return this.value
+      },
+      set(newValue) {
+        if (this.value !== newValue) {
+          this.$emit("onChange", newValue)
+        }
+      }
+    }
+  }
+}
+</script>
