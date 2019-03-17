22280d71c04b021585f1b7ca7febfff6cb669a07
autopp <autopp.inc@gmail.com>
2019-03-17 18:38:03 +0900

    feat: define KingdomInput as molecule

---
+++ components/molecules/KingdomInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    {{ name }}
+    <AppNumberInput :value="value" @onChange="onChange" />
+  </div>
+</template>
+
+<script>
+import AppNumberInput from '@/components/atoms/AppNumberInput'
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    onChange(value) {
+      this.$emit("onChange", value)
+    }
+  },
+  components: {
+    AppNumberInput
+  }
+}
+</script>
