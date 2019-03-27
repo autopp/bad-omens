876262c21aa54deb934f79e2d292377539ecdd6e
autopp <autopp.inc@gmail.com>
2019-03-27 09:47:21 +0900

    feat: define props for SupplyPanel

---
+++ components/molecules/SupplyPanel.vue
@@ -0,0 +1,39 @@
+<template>
+  <div />
+</template>
+
+<script>
+import VueTypes from "vue-types";
+
+export default {
+  props: {
+    supply: VueTypes.shape({
+      kingdom: VueTypes.objectOf(
+        VueTypes.arrayOf(
+          VueTypes.shape({
+            id: VueTypes.integer.isRequired,
+            name: VueTypes.string.isRequired,
+            heirloom: VueTypes.string,
+            druid: VueTypes.bool,
+            boon: VueTypes.bool,
+            hex: VueTypes.bool,
+            willOWisp: VueTypes.bool,
+            imp: VueTypes.bool,
+            ghost: VueTypes.bool,
+            wish: VueTypes.bool,
+            zombie: VueTypes.bool,
+            bat: VueTypes.bool
+          })
+        )
+      ),
+      druidBoons: VueTypes.arrayOf(
+        VueTypes.shape({
+          id: VueTypes.integer.isRequired,
+          name: VueTypes.string.isRequired,
+          willOWisp: VueTypes.bool
+        })
+      )
+    })
+  }
+};
+</script>
