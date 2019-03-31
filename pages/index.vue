f066140644f069ae1a5c1a05a675d40182382877
autopp <autopp.inc@gmail.com>
2019-03-31 17:46:20 +0900

    style: apply vuetify to layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,14 @@
 <template>
-  <section class="container">
-    <div>
-      <h1 class="title">
-        Bad Omens
-      </h1>
+  <main>
+    <v-toolbar><v-toolbar-title>Bad Omens</v-toolbar-title></v-toolbar>
+    <v-container fluid>
       <KingdomInput />
       <AppButton label="生成" @onClick="onGenerate" />
       <div v-for="(supply, index) in $store.state.supplies" :key="index">
         <SupplyPanel :supply="supply" />
       </div>
-    </div>
-  </section>
+    </v-container>
+  </main>
 </template>
 
 <script>
@@ -31,35 +29,3 @@ export default {
   }
 };
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
