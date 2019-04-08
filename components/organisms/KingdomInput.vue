fa684f6f19c62a8854b30e06f3e71c5f6af38851
autopp <autopp.inc@gmail.com>
2019-04-08 09:54:40 +0900

    feat: validate number input

--- components/organisms/KingdomInput.vue
+++ components/organisms/KingdomInput.vue
@@ -39,7 +39,21 @@ export default {
       this.$store.commit("changeBaseValue", 10 - parseInt(value));
     },
     onGenerate() {
+      const errors = this.validateForGenerate();
+      if (errors.length > 0) {
+        this.$store.commit("setErrors", errors);
+        return;
+      }
+
       this.$store.commit("setDummySupplies");
+    },
+    validateForGenerate() {
+      const errors = [];
+      if (this.$store.state.baseValue < 0 || this.$store.state.baseValue > 10) {
+        errors.push("王国カードの数が不正です");
+      }
+
+      return errors;
     }
   }
 };
